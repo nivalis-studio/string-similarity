d97cdcf5c54d4511464235edff52a1aa080ecdaa
Paul Nodet <paul.nodet@gmail.com>
2025-06-26 22:28:43 +0200

    refactor: convert js to typescript

--- index.spec.ts
+++ index.spec.ts
@@ -1,103 +1,153 @@
-var stringSimilarity = require('./index');
+/* eslint-disable @typescript-eslint/no-magic-numbers */
+import { describe, expect, it } from 'bun:test';
+import { compareTwoStrings, findBestMatch } from './index';
 
-describe('compareTwoStrings', function () {
-  var compareTwoStrings = stringSimilarity.compareTwoStrings;
-
-  it('is a function', function () {
+describe('compareTwoStrings', () => {
+  it('is a function', () => {
     expect(typeof compareTwoStrings).toEqual('function');
   });
 
-  it('returns the correct value for different inputs:', function () {
+  it('returns the correct value for different inputs:', () => {
     const testData = [
       { first: 'french', second: 'quebec', expected: 0 },
       { first: 'france', second: 'france', expected: 1 },
       { first: 'fRaNce', second: 'france', expected: 0.2 },
       { first: 'healed', second: 'sealed', expected: 0.8 },
-      { first: 'web applications', second: 'applications of the web', expected: 0.7878787878787878 },
-      { first: 'this will have a typo somewhere', second: 'this will huve a typo somewhere', expected: 0.92 },
-      { first: 'Olive-green table for sale, in extremely good condition.', second: 'For sale: table in very good  condition, olive green in colour.', expected: 0.6060606060606061 },
-      { first: 'Olive-green table for sale, in extremely good condition.', second: 'For sale: green Subaru Impreza, 210,000 miles', expected: 0.2558139534883721 },
-      { first: 'Olive-green table for sale, in extremely good condition.', second: 'Wanted: mountain bike with at least 21 gears.', expected: 0.1411764705882353 },
-      { first: 'this has one extra word', second: 'this has one word', expected: 0.7741935483870968 },
+      {
+        first: 'web applications',
+        second: 'applications of the web',
+        expected: 0.787_878_787_878_787_8,
+      },
+      {
+        first: 'this will have a typo somewhere',
+        second: 'this will huve a typo somewhere',
+        expected: 0.92,
+      },
+      {
+        first: 'Olive-green table for sale, in extremely good condition.',
+        second:
+          'For sale: table in very good  condition, olive green in colour.',
+        expected: 0.606_060_606_060_606_1,
+      },
+      {
+        first: 'Olive-green table for sale, in extremely good condition.',
+        second: 'For sale: green Subaru Impreza, 210,000 miles',
+        expected: 0.255_813_953_488_372_1,
+      },
+      {
+        first: 'Olive-green table for sale, in extremely good condition.',
+        second: 'Wanted: mountain bike with at least 21 gears.',
+        expected: 0.141_176_470_588_235_3,
+      },
+      {
+        first: 'this has one extra word',
+        second: 'this has one word',
+        expected: 0.774_193_548_387_096_8,
+      },
       { first: 'a', second: 'a', expected: 1 },
       { first: 'a', second: 'b', expected: 0 },
       { first: '', second: '', expected: 1 },
       { first: 'a', second: '', expected: 0 },
       { first: '', second: 'a', expected: 0 },
       { first: 'apple event', second: 'apple    event', expected: 1 },
-      { first: 'iphone', second: 'iphone x', expected: 0.9090909090909091 }
+      {
+        first: 'iphone',
+        second: 'iphone x',
+        expected: 0.909_090_909_090_909_1,
+      },
     ];
 
-    testData.forEach(td => {
-      expect(compareTwoStrings(td.first, td.second)).toBe(td.expected, td);
-    });
+    for (const td of testData) {
+      expect(compareTwoStrings(td.first, td.second)).toBe(td.expected);
+    }
   });
 });
 
-describe('findBestMatch', function () {
-  var findBestMatch = stringSimilarity.findBestMatch;
-  var badArgsErrorMsg = 'Bad arguments: First argument should be a string, second should be an array of strings';
+describe('findBestMatch', () => {
+  const badArgsErrorMsg =
+    'Bad arguments: First argument should be a string, second should be an array of strings';
 
-  it('is a function', function () {
+  it('is a function', () => {
     expect(typeof findBestMatch).toBe('function');
   });
 
-  it('accepts a string and an array of strings and returns an object', function () {
-    var output = findBestMatch('one', ['two', 'three']);
+  it('accepts a string and an array of strings and returns an object', () => {
+    const output = findBestMatch('one', ['two', 'three']);
+
     expect(typeof output).toBe('object');
   });
 
-  it("throws a 'Bad arguments' error if no arguments passed", function () {
-    expect(function () {
+  it("throws a 'Bad arguments' error if no arguments passed", () => {
+    expect(() => {
+      // @ts-expect-error Invalid arguments
       findBestMatch();
     }).toThrowError(badArgsErrorMsg);
   });
 
-  it("throws a 'Bad arguments' error if first argument is not a non-empty string", function () {
-    expect(function () {
+  it("throws a 'Bad arguments' error if first argument is not a non-empty string", () => {
+    expect(() => {
+      // @ts-expect-error Invalid arguments
       findBestMatch('');
     }).toThrowError(badArgsErrorMsg);
 
-    expect(function () {
+    expect(() => {
+      // @ts-expect-error Invalid arguments
       findBestMatch(8);
     }).toThrowError(badArgsErrorMsg);
   });
 
-  it("throws a 'Bad arguments' error if second argument is not an array with at least one element", function () {
-    expect(function () {
+  it("throws a 'Bad arguments' error if second argument is not an array with at least one element", () => {
+    expect(() => {
+      // @ts-expect-error Invalid arguments
       findBestMatch('hello', 'something');
     }).toThrowError(badArgsErrorMsg);
 
-    expect(function () {
+    expect(() => {
       findBestMatch('hello', []);
     }).toThrowError(badArgsErrorMsg);
   });
 
-  it("throws a 'Bad arguments' error if second argument is not an array of strings", function () {
-    expect(function () {
+  it("throws a 'Bad arguments' error if second argument is not an array of strings", () => {
+    expect(() => {
+      // @ts-expect-error Invalid arguments
       findBestMatch('hello', [2, 'something']);
     }).toThrowError(badArgsErrorMsg);
   });
 
-  it('assigns a similarity rating to each string passed in the array', function () {
-    var matches = findBestMatch('healed', ['mailed', 'edward', 'sealed', 'theatre']);
+  it('assigns a similarity rating to each string passed in the array', () => {
+    const matches = findBestMatch('healed', [
+      'mailed',
+      'edward',
+      'sealed',
+      'theatre',
+    ]);
 
     expect(matches.ratings).toEqual([
       { target: 'mailed', rating: 0.4 },
       { target: 'edward', rating: 0.2 },
       { target: 'sealed', rating: 0.8 },
-      { target: 'theatre', rating: 0.36363636363636365 }
+      { target: 'theatre', rating: 0.363_636_363_636_363_65 },
     ]);
   });
 
-  it("returns the best match and its similarity rating", function () {
-    var matches = findBestMatch('healed', ['mailed', 'edward', 'sealed', 'theatre']);
+  it('returns the best match and its similarity rating', () => {
+    const matches = findBestMatch('healed', [
+      'mailed',
+      'edward',
+      'sealed',
+      'theatre',
+    ]);
 
     expect(matches.bestMatch).toEqual({ target: 'sealed', rating: 0.8 });
   });
 
-  it("returns the index of best match from the target strings", function () {
-    var matches = findBestMatch('healed', ['mailed', 'edward', 'sealed', 'theatre']);
+  it('returns the index of best match from the target strings', () => {
+    const matches = findBestMatch('healed', [
+      'mailed',
+      'edward',
+      'sealed',
+      'theatre',
+    ]);
 
     expect(matches.bestMatchIndex).toBe(2);
   });
--- index.ts
+++ index.ts
@@ -1,66 +1,81 @@
-module.exports = {
-	compareTwoStrings:compareTwoStrings,
-	findBestMatch:findBestMatch
-};
+/* eslint-disable @typescript-eslint/no-magic-numbers */
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+/* eslint-disable unicorn/prefer-string-slice */
+/* eslint-disable no-param-reassign */
+/* eslint-disable func-style */
 
-function compareTwoStrings(first, second) {
-	first = first.replace(/\s+/g, '')
-	second = second.replace(/\s+/g, '')
+function areArgsValid(mainString: string, targetStrings: string[]): boolean {
+  if (typeof mainString !== 'string') return false;
 
-	if (first === second) return 1; // identical or empty
-	if (first.length < 2 || second.length < 2) return 0; // if either is a 0-letter or 1-letter string
+  if (!Array.isArray(targetStrings)) return false;
 
-	let firstBigrams = new Map();
-	for (let i = 0; i < first.length - 1; i++) {
-		const bigram = first.substring(i, i + 2);
-		const count = firstBigrams.has(bigram)
-			? firstBigrams.get(bigram) + 1
-			: 1;
+  if (targetStrings.length === 0) return false;
 
-		firstBigrams.set(bigram, count);
-	};
-
-	let intersectionSize = 0;
-	for (let i = 0; i < second.length - 1; i++) {
-		const bigram = second.substring(i, i + 2);
-		const count = firstBigrams.has(bigram)
-			? firstBigrams.get(bigram)
-			: 0;
-
-		if (count > 0) {
-			firstBigrams.set(bigram, count - 1);
-			intersectionSize++;
-		}
-	}
-
-	return (2.0 * intersectionSize) / (first.length + second.length - 2);
+  return !targetStrings.some(str => typeof str !== 'string');
 }
 
-function findBestMatch(mainString, targetStrings) {
-	if (!areArgsValid(mainString, targetStrings)) throw new Error('Bad arguments: First argument should be a string, second should be an array of strings');
-	
-	const ratings = [];
-	let bestMatchIndex = 0;
+function compareTwoStrings(first: string, second: string): number {
+  first = first.replaceAll(/\s+/g, '');
+  second = second.replaceAll(/\s+/g, '');
 
-	for (let i = 0; i < targetStrings.length; i++) {
-		const currentTargetString = targetStrings[i];
-		const currentRating = compareTwoStrings(mainString, currentTargetString)
-		ratings.push({target: currentTargetString, rating: currentRating})
-		if (currentRating > ratings[bestMatchIndex].rating) {
-			bestMatchIndex = i
-		}
-	}
-	
-	
-	const bestMatch = ratings[bestMatchIndex]
-	
-	return { ratings: ratings, bestMatch: bestMatch, bestMatchIndex: bestMatchIndex };
+  if (first === second) return 1; // identical or empty
+
+  if (first.length < 2 || second.length < 2) return 0; // if either is a 0-letter or 1-letter string
+
+  const firstBigrams = new Map<string, number>();
+
+  for (let i = 0; i < first.length - 1; i++) {
+    const bigram = first.substring(i, i + 2);
+    const count = firstBigrams.has(bigram) ? firstBigrams.get(bigram)! + 1 : 1;
+
+    firstBigrams.set(bigram, count);
+  }
+
+  let intersectionSize = 0;
+
+  for (let i = 0; i < second.length - 1; i++) {
+    const bigram = second.substring(i, i + 2);
+    const count: number = firstBigrams.has(bigram)
+      ? firstBigrams.get(bigram)!
+      : 0;
+
+    if (count > 0) {
+      firstBigrams.set(bigram, count - 1);
+
+      intersectionSize += 1;
+    }
+  }
+
+  return (2 * intersectionSize) / (first.length + second.length - 2);
 }
 
-function areArgsValid(mainString, targetStrings) {
-	if (typeof mainString !== 'string') return false;
-	if (!Array.isArray(targetStrings)) return false;
-	if (!targetStrings.length) return false;
-	if (targetStrings.find( function (s) { return typeof s !== 'string'})) return false;
-	return true;
+function findBestMatch(mainString: string, targetStrings: string[]) {
+  if (!areArgsValid(mainString, targetStrings)) {
+    throw new Error(
+      'Bad arguments: First argument should be a string, second should be an array of strings',
+    );
+  }
+
+  const ratings = [];
+  let bestMatchIndex = 0;
+
+  for (const [i, currentTargetString] of targetStrings.entries()) {
+    const currentRating = compareTwoStrings(mainString, currentTargetString);
+
+    ratings.push({ target: currentTargetString, rating: currentRating });
+
+    if (currentRating > ratings[bestMatchIndex]!.rating) {
+      bestMatchIndex = i;
+    }
+  }
+
+  const bestMatch = ratings[bestMatchIndex];
+
+  return {
+    ratings,
+    bestMatch,
+    bestMatchIndex,
+  };
 }
+
+export { compareTwoStrings, findBestMatch };
